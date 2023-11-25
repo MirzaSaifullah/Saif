dc00d920ef77e532256ddab83d3a358f492e88bc
Mirza Saifullah <140934766+MirzaSaifullah@users.noreply.github.com>
2023-11-24 21:54:06 -0800

    Create Arthimatic_Operators.py
    I hope this will be helpful for you

---
+++ Arthimatic_Operators.py
@@ -0,0 +1,23 @@
+a = 10
+b = 3
+
+# + (Addition)
+print(a + b)  # 13
+
+# - (Subtraction)
+print(a - b)  # 7
+
+# * (Multiplication)
+print(a * b)  # 30
+
+# / (Division)
+print(a / b)  # 3.3333...
+
+# % (Modulus)
+print(a % b)  # 1
+
+# ** (Exponentiation) power
+print(a ** b) # 1000
+
+# // (Floor Division)
+print(a // b) # 3
